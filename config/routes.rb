47f4145b26c7caf5102f45eaf8cf5f76285b30ef
katievanover <katie_vanover@hotmail.com>
2019-09-24 10:49:28 -0600

    complete authenication system

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
  resources :articles
  resources :users
 
+
  get 'signup', to: 'users#new'
  post 'users', to: 'users#create'
+
+ get 'login', to: 'sessions#new'
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
 
 end
